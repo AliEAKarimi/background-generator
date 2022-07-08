9ca25cfe47f1b49a61de12904ec6a11736f1fd25
alieakarimi <alieakarimi@gmail.com>
2022-07-09 02:17:25 +0430

    add random background generator button

--- script.js
+++ script.js
@@ -2,18 +2,27 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector("#color1");
 var color2 = document.querySelector("#color2");
 var body = document.getElementById("gradient");
+let randomColorGenerator = document.querySelector("#random_color_generator");
 
-function setGradient(){
+function setGradient(firstColor=color1.value, secondColor=color2.value){
     body.style.background = "linear-gradient(to right, "
-    + color1.value
+    + firstColor
     + ", "
-    + color2.value
+    + secondColor
     + ")";
     //css.textContent = body.style.background + ";";
     css.appendChild(document.createTextNode(body.style.background + ";"));
 }
 
+function createRandomColor() {
+    return "#" + Math.floor(Math.random()*16777215).toString(16);
+}
+
+function setRandomBackgroundColor(){
+    setGradient(createRandomColor(), createRandomColor());
+}
+
 setGradient();
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
-
+randomColorGenerator.addEventListener("click", setRandomBackgroundColor);
